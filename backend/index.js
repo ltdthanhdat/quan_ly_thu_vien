96c4c44af090bcd32200918a16a7895958b61333
thanhdat <lethanhdat.real@gmail.com>
2024-04-29 12:41:20 +0700

    server update

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 import express from 'express'
+import readerRouter from './routes/reader.route.js'
+import bookRouter from './routes/book.route.js'
 
 const app = express()
 const PORT = 3000
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
-    res.send('hello world')
+    res.json('hello world')
 })
 
 app.use('/readers', readerRouter)
