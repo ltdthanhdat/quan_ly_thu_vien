8b424eff11af14f7d69b3e154a32ceaed4802ba0
thanhdat <lethanhdat.real@gmail.com>
2024-05-27 19:13:14 +0700

    update pages routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,32 @@
 import { Routes, Route, Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import Books from './pages/Books'
+import Readers from './pages/Readers'
+import CreateUser from './pages/CreateUser'
 import './index.css'
+import InsertBook from './pages/InsertBook'
 
 const App = () => {
   const [isOpen, setIsOpen] = useState(true)
-  const [width, setWidth] = useState(window.innerWidth)
 
   const toggle = () => {
     setIsOpen(!isOpen)
   }
-  useEffect(() => {
-    const handleResize = () => {
-      setWidth(window.innerWidth)
-    }
-    window.addEventListener('resize', handleResize)
-    return () => {
-      window.removeEventListener('resize', handleResize)
-    }
-  }, [])
+
 
   return (
     <>
       <div className="app row">
-        <nav className={`collapse min-vw-30 vh-100 bg-light col-2 p-4 ${isOpen && width >= 500 ? 'show' : ''}`}>
+        <nav className={`collapse w-30 vh-100 bg-light col-xs-10 col-sm-2 p-4 ${isOpen ? 'show' : ''}`}>
           <div className='pb-3'><Link className='text-decoration-none text-primary h6' to="/">Home</Link></div>
 
           <h6>Quản lý độc giả</h6>
           <div><Link className='text-decoration-none text-muted' to="/lam-the-thu-vien">Làm thẻ thư viện</Link></div>
-          <div><Link className='text-decoration-none text-muted' to="/quan-ly-thong-tin">Quản lý thông tin</Link></div>
+          <div><Link className='text-decoration-none text-muted' to="/doc-gia">Quản lý thông tin</Link></div>
           <br />
 
           <h6>Quản lý sách</h6>
-          <div><Link className='text-decoration-none text-muted' to="/">Cập nhật sách mới</Link></div>
+          <div><Link className='text-decoration-none text-muted' to="/them-sach">Cập nhật sách mới</Link></div>
           <div><Link className='text-decoration-none text-muted' to="/sach">Tìm kiếm sách</Link></div>
           <br />
 
@@ -49,13 +43,15 @@ const App = () => {
           <div><Link className='text-decoration-none text-muted' to="/">Thống kê quá hạn</Link></div>
         </nav>
 
-        <div className='vh-100 bg-body col'>
+        <div className='vh-100 bg-body col-xs-2 col-sm-10'>
           <div>
             <button className='bg-info' onClick={toggle}>Menu</button>
-            <h2 className='bg-info-subtle text-center'>{`Title ${width}`}</h2>
           </div>
           <Routes>
             <Route path="/sach" element={<Books />} />
+            <Route path="/doc-gia" element={<Readers />} />
+            <Route path="/lam-the-thu-vien" element={<CreateUser />} />
+            <Route path="/them-sach" element={<InsertBook />} />
           </Routes>
         </div>
 
