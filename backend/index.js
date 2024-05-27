e25dfa00416463299eb4f6d595367202998ad689
thanhdat <lethanhdat.real@gmail.com>
2024-05-27 19:07:39 +0700

    add publisher router

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,22 @@
 import express from 'express'
+import cors from 'cors'
 import readerRouter from './routes/reader.route.js'
 import bookRouter from './routes/book.route.js'
+import publisherRouter from './routes/publisher.route.js'
 
 const app = express()
 const PORT = 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
 app.get('/', (req, res) => {
-    res.json('hello world')
+    res.json('homepage')
 })
 
 app.use('/readers', readerRouter)
 app.use('/books', bookRouter)
+app.use('/publishers', publisherRouter)
 
 app.listen(PORT)
