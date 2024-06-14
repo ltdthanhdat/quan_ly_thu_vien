0fdb1693a63755f77552e90817b727b4c8442b05
thanhdat <lethanhdat.real@gmail.com>
2024-06-14 15:01:33 +0700

    add search publisher router

--- backend/controllers/publisher.controller.js
+++ backend/controllers/publisher.controller.js
@@ -14,4 +14,26 @@ const getAllPublishers = (req, res) => {
     )
 }
 
-export default { getAllPublishers }
+const findPublisher = (req, res) => {
+    const { TenNXB } = req.body; // Giả sử thông tin TenNXB được gửi trong body request
+
+    const sql = 'SELECT MaNXB FROM NhaXuatBan WHERE TenNXB = ?';
+    db.query(
+        sql,
+        [TenNXB],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+                res.json('error');
+            } else {
+                if (result.length > 0) {
+                    res.json(result[0]); // Trả về kết quả đầu tiên nếu có
+                } else {
+                    res.json({ message: 'Không tìm thấy NXB nào với tên này' });
+                }
+            }
+        }
+    );
+}
+
+export default { getAllPublishers, findPublisher }
--- backend/routes/publisher.route.js
+++ backend/routes/publisher.route.js
@@ -3,5 +3,6 @@ import pubCtrls from '../controllers/publisher.controller.js'
 const router = express.Router()
 
 router.get('/', pubCtrls.getAllPublishers)
+router.post('/find', pubCtrls.findPublisher)
 
 export default router
